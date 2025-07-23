dbd139b8bba21905b4dc739a7e4757fa36acf35b
Steven Hertz <StevenH@appi4.com>
2025-07-23 16:56:18 -0400

    Initial Commit

---
+++ MyPushDemoUPDT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPushDemoUPDT
+//
+//  Created by Steven Hertz on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPushDemoUPDT/MyPushDemoUPDTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPushDemoUPDTApp.swift
+//  MyPushDemoUPDT
+//
+//  Created by Steven Hertz on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPushDemoUPDTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
